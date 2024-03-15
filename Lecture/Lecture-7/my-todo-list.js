293d568d008bc8b79ca62fddbd1068215f2ebdb5
Briarre Johnson <briarrej@umich.edu>
2024-03-15 13:33:16 -0400

    x

--- Lecture/Lecture-7/my-todo-list.js
+++ Lecture/Lecture-7/my-todo-list.js
@@ -1,12 +1,24 @@
 // TODO: Write your code here
 function addNewItem(content) {
-    const L = document.createElement("li");
-    L.innerText = content;
-    L.classList.add('#item');
-
     const parent = document.querySelector('#items');
-    parent.append(L);
-    
+    const newItem = document.createElement("li");
+    newItem.innerText = content;
+    newItem.classList.add('item');
 
+    // Toggle 'done' class when the item is clicked
+    newItem.addEventListener('click', () => {
+        newItem.classList.toggle('done');
+    });
+
+    parent.appendChild(newItem);
 }
-addNewItem("Print documents")
+const inputElement = document.querySelector('#my-inp');
+inputElement.addEventListener('keydown', (event) => {
+    const key = event.key;
+    if(key === 'Enter') {
+        const content = inputElement.value;
+        addNewItem(content);
+        inputElement.value = '';
+    }
+});
+//addNewItem("Print documents")
---
+++ Problem-Sets/timer/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          What do you need to do? Let's make a list!
+        </p>
+    </div>
+  );
+}
+
+export default App;
