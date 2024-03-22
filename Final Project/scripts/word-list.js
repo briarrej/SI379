34df2dbb745225310c836bb3213934574b3e5e92
Briarre Johnson <briarrej@umich.edu>
2024-03-22 13:00:55 -0400

    final project code

---
+++ Final Project/scripts/word-list.js
@@ -0,0 +1,82 @@
+const wordList = [
+    {
+        word: "degree",
+        hint: "An academic qualification conferred by a college or university upon completion of a program of study."
+    },
+    {
+        word: "graduation",
+        hint: "The receiving or conferring of an academic degree or diploma marking the completion of studies."
+    },
+    {
+        word: "alumni",
+        hint: "Graduates or former students of a particular educational institution."
+    },
+    {
+        word: "scholarship",
+        hint: "Financial aid awarded to a student to subsidize their education, typically based on academic or other achievements."
+    },
+    {
+        word: "cap and gown",
+        hint: "The traditional attire worn by graduates during commencement ceremonies, consisting of a cap and gown."
+    },
+    {
+        word: "thesis",
+        hint: "A long essay or dissertation involving personal research, written by a candidate for a university degree."
+    },
+    {
+        word: "classmate",
+        hint: "A person who is in the same class or course of study as another student."
+    },
+    {
+        word: "lecture",
+        hint: "An educational talk to an audience, especially one of students in a university or college."
+    },
+    {
+        word: "homework",
+        hint: "Tasks assigned to students by teachers to be completed outside of class, typically as practice or reinforcement of learning."
+    },
+    {
+        word: "major",
+        hint: "The primary field of study pursued by a student in college or university, typically leading to a degree."
+    },
+    {
+        word: "wolverines",
+        hint: "The mascot of the University of Michigan, known for its ferocity and resilience."
+    },
+    {
+        word: "Trotter",
+        hint: "This is the multicultural center on campus where minority students on campus go to feel a safe space on campus."
+    },
+    {
+        word: "mden",
+        hint: "Students and fans go here to purchase U of M gear and buy the latests championship clothing."
+    },
+    {
+        word: "SI",
+        hint: "This is the abbreviation of the best program at the University of Michigan. Students study the power of information in this program."
+    },
+    {
+        word: "maize",
+        hint: "One of the official colors of the University of Michigan, a bright yellowish-gold color."
+    },
+    {
+        word: "blue",
+        hint: "One of the official colors of the University of Michigan, a deep shade of blue."
+    },
+    {
+        word: "hail",
+        hint: "A traditional greeting and expression of support among students and alumni of the University of Michigan."
+    },
+    {
+        word: "lsa",
+        hint: "This building on campus is home to the Literature, Science, and Arts students and it is on State St."
+    },
+    {
+        word: "diag",
+        hint: "A central open space on the University of Michigan campus, known for its role as a gathering place and hub of activity."
+    },
+    {
+        word: "victors",
+        hint: "The fight song of the University of Michigan, played at athletic events and other occasions to celebrate achievements."
+    }
+];
